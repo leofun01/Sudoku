8ec70fc028b302a252254f161bb69f21e2b56c0b
leofun01 <leofun01@gmail.com>
2020-08-16 14:36:36 +0300

    Add EmptyCell for IBoard

--- DotNetSudoku/IBoard.cs
+++ DotNetSudoku/IBoard.cs
@@ -4,11 +4,12 @@ using System.Collections.ObjectModel;
 namespace DotNetSudoku {
 	using R = IOrderedSet<int>;
 	using RC = ReadOnlyCollection<IOrderedSet<int>>;
-
+	
 	public interface IBoard<C> : IEnumerable<C>
 		where C : ICell<C>, new()
 	{
 		RC Regions { get; }
+		C EmptyCell { get; }
 		int CellsCount { get; }
 		C this[int i] { get; set; }
 		IBoard<C> GetClone();
